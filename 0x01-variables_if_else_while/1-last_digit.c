9fa2a0828a6ddb1e73aa0dfcb19452addc978f25
Erick <oderoerique@gmail.com>
2023-02-20 11:05:10 -0800

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -2,9 +2,9 @@
 #include <time.h>
 #include <stdio.h>
 /**
-*main-C code progam that assign variable  
-*Return:0 (success)
-*/
+ * main -C code progam that assign variable  
+ * Return:0 (success)
+ */
 int main(void)
 {
 int n;
